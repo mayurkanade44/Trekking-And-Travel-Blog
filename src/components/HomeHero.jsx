cfe91108e06e33902c3c3a4a4b74e4738f5faac4
mayurkanade44 <mayurkanade44@gmail.com>
2021-07-26 15:40:36 +0530

    home page

--- src/components/HomeHero.jsx
+++ src/components/HomeHero.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const HomeHero = () => {
-    return (
-        <div>
-            home hero
-        </div>
-    )
-}
+  return (
+    <div className="home-hero">
+      <h1 className="search-header">Indian Community Of Nomads</h1>
+      <div className="input-group input-group-lg mb-5 search-bar">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Search for travel blogs, trek blogs or photo blogs"
+        />
+        <button className="btn btn-dark" type="button">
+          Search
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default HomeHero
+export default HomeHero;
